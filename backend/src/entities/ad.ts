e4864ffa58a7c26eeca2276464ae8e3f084b0194
Samuel Faber <samuel.faber@live.fr>
2023-09-22 14:57:23 +0200

    Ajout de la propriété 'nullable' sur la colonne 'description'

--- backend/src/entities/ad.ts
+++ backend/src/entities/ad.ts
@@ -7,11 +7,11 @@ export class Ad extends BaseEntity {
   id: number;
 
   @Column()
-  @Length(4, 100, { message: " Entre 4 et 100 caractères" })
+  @Length(4, 100, { message: "Entre 4 et 100 caractères" })
   title: string;
 
-  @Column()
-  @Length(20, 500, { message: " Entre 20 et 500 caractères" })
+  @Column({ nullable: true })
+  @Length(20, 500, { message: "Entre 20 et 500 caractères" })
   description: string;
 
   @Column()
