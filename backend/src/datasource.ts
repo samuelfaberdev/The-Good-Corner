014a8b95818d1035b9522311f448387aa4dba3b8
Samuel Faber <samuel.faber@live.fr>
2024-01-05 11:09:47 +0100

    Update database configuration in datasource.ts

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -1,12 +1,18 @@
+import "dotenv/config";
 import { DataSource } from "typeorm";
 import { Ad } from "./entities/Ad";
 import { Category } from "./entities/Category";
 import { Tag } from "./entities/Tag";
+import { User } from "./entities/User";
 
 export const dataSource = new DataSource({
-  type: "sqlite",
-  database: "./tgc.sqlite",
-  entities: [Ad, Category, Tag],
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [Ad, Category, Tag, User],
   synchronize: true,
   logging: true,
 });
